067d4a1a08b13f721b6fcd2ab8892046224711d5
Marco Borelli <marco.borelli@epfl.ch>
2019-05-15 17:42:21 +0200

    Add one test for deprecated keys parser option

--- tests/parsers/test_pw.py
+++ tests/parsers/test_pw.py
@@ -64,6 +64,12 @@ def generate_inputs_vcrelax_Ge2():
         'settings': orm.Dict()
     })
 
+# @pytest.fixture
+# def generate_inputs_vcrelax_Ge2_deprkeys():
+#     inputs = generate_inputs_vcrelax_Ge2()
+#     inputs.settings.include_deprecated_v2_keys = True
+#     return inputs
+
 
 def test_pw_default(fixture_database, fixture_computer_localhost, generate_calc_job_node, generate_parser,
                     generate_inputs, data_regression):
@@ -85,8 +91,8 @@ def test_pw_default(fixture_database, fixture_computer_localhost, generate_calc_
     data_regression.check(results['output_parameters'].get_dict())
 
 
-def test_pw_vcrelax_Ge2_qe63_oldxml(fixture_database, fixture_computer_localhost, generate_calc_job_node, generate_parser,
-                    generate_inputs_vcrelax_Ge2, data_regression):
+def test_pw_vcrelax_Ge2_qe63_oldxml(fixture_database, fixture_computer_localhost, generate_calc_job_node,
+                                    generate_parser, generate_inputs_vcrelax_Ge2, data_regression):
     """Test a default `pw.x` calculation.
 
     The output is created by running a dead simple SCF calculation for a silicon structure.
@@ -108,8 +114,8 @@ def test_pw_vcrelax_Ge2_qe63_oldxml(fixture_database, fixture_computer_localhost
     )
 
 
-def test_pw_vcrelax_Ge2_qe63_newxml(fixture_database, fixture_computer_localhost, generate_calc_job_node, generate_parser,
-                    generate_inputs_vcrelax_Ge2, data_regression):
+def test_pw_vcrelax_Ge2_qe63_newxml(fixture_database, fixture_computer_localhost, generate_calc_job_node,
+                                    generate_parser, generate_inputs_vcrelax_Ge2, data_regression):
     """Test a default `pw.x` calculation.
 
     The output is created by running a dead simple SCF calculation for a silicon structure.
@@ -129,3 +135,36 @@ def test_pw_vcrelax_Ge2_qe63_newxml(fixture_database, fixture_computer_localhost
     data_regression.check(
         results['output_parameters'].get_dict(),
     )
+
+def test_pw_vcrelax_Ge2_qe63_newxml_deprkeys(fixture_database, fixture_computer_localhost, generate_calc_job_node,
+                                             generate_parser, generate_inputs_vcrelax_Ge2, data_regression):
+    """Test the new XML parser with 'include_deprecated_v2_keys' option, versus without."""
+    entry_point_calc_job = 'quantumespresso.pw'
+    entry_point_parser = 'quantumespresso.pw'
+
+    inputs = generate_inputs_vcrelax_Ge2
+    inputs.settings.update_dict({'parser_options': {'include_deprecated_v2_keys': True}})
+    node = generate_calc_job_node(entry_point_calc_job, fixture_computer_localhost, 'vcrelax_Ge2_qe63_newxml', inputs)
+    parser = generate_parser(entry_point_parser)
+    results, calcfunction = parser.parse_from_node(node, store_provenance=False)
+
+    assert calcfunction.is_finished, calcfunction.exception
+    assert calcfunction.is_finished_ok, calcfunction.exit_message
+    assert 'output_parameters' in results
+    
+    # Remove and check the extra deprecated keys
+    out_params = results['output_parameters'].get_dict()
+    assert out_params.pop('fixed_occupations') == False
+    assert out_params.pop('smearing_method') == True
+    assert out_params.pop('tetrahedron_method') == False
+    
+    data_regression.check(
+        out_params,
+        basename='test_pw_vcrelax_Ge2_qe63_newxml'
+    )
+    
+# TODO:
+#  - check other outputs (structure, bands, kpoints, trajectory, ...)
+#   (run this on the aiida_0.12 environment: meld <(verdi run print_results.py 4015) <(verdi run print_results.py 441)
+#  - check old aiida-qe 2.1 vs 3.0
+#  - explicitly check the differences between old XML and new XML
